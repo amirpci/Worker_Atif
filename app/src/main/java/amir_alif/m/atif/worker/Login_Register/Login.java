823d462ee23c5f3c560a8aed77cafc5e0fdd83f1
Amir Mu'tashim Billah <amirmutashim@gmail.com>
2018-04-21 01:32:47 +0700

    permulaan (0)
    gurung ngoding

---
+++ app/src/main/java/amir_alif/m/atif/worker/Login_Register/Login.java
@@ -0,0 +1,15 @@
+package amir_alif.m.atif.worker.Login_Register;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import amir_alif.m.atif.worker.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
