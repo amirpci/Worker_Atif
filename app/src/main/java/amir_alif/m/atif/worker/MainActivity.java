1a2be5251c761782a926f742ab1f215a91212d06
Amir Mu'tashim Billah <amirmutashim@gmail.com>
2018-04-21 01:25:03 +0700

    Initial commit

---
+++ app/src/main/java/amir_alif/m/atif/worker/MainActivity.java
@@ -0,0 +1,13 @@
+package amir_alif.m.atif.worker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
